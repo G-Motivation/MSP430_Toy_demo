7fb6cd290e40ed3808ad8ad9970d09f531ba8f9a
yaodehaung <sef1548@gmail.com>
2024-04-11 14:40:25 +0800

    add sysSetup function into main

--- hello.c
+++ hello.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
-#include <msp430.h> 
+#include <msp430.h>
+#include <sysSetup.h>
 
 
+//  Vin= 1V-3V -----------------
+//     |      |                 |
+//    10K     |                 |
+//     |      |                 |
+//     |      |                 |
+//     -------| A2=2/3Vin       |
+//     |      |                 |
+//     10K    |                 |
+//     |      |                 |
+//     -------| A3=1/3Vin       |
+//     |      |                 |
+//    10K     |             P1.0|-->LED
+//     |      |                 |
+//    GND
 /**
  * hello.c
  */
 int main(void)
 {
 
-	
-    WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
+    sysSetup();
+                                            // Lock CS registers
 
-    // Configure GPIO
-    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
-    P1DIR |= BIT0;                          // Set P1.0 to output direction
-
-    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
     printf("Hello World!\n");
 
-    while(1)
+    while (1)
     {
-        P1OUT ^= BIT0;                      // Toggle LED
+        P1OUT ^= BIT0; // Toggle LED
         __delay_cycles(100000);
     }
 
-	
-	return 0;
+    return 0;
 }
