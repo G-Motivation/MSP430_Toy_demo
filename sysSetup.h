24865c8d21883086b586e913824a75057572c3b6
yaodehaung <sef1548@gmail.com>
2024-04-11 14:31:08 +0800

    add sysSetup

---
+++ sysSetup.h
@@ -0,0 +1,17 @@
+// ****************************************************************************
+// List of Includes
+// ****************************************************************************
+// ****************************************************************************
+// sysSetup
+// @brief This function performs the board initialization. This includes:
+// clock, I/O interfaces, adc, charger, monitor, heater.
+// @param none
+// @return none
+// ****************************************************************************
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+void sysSetup(
+    void
+    );
+
+#endif /* SYSTEM_H_ */
