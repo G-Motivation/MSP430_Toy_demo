217166749dc8e58b99e77b1367d11f6106ad5a2a
yaodehaung <sef1548@gmail.com>
2024-04-15 16:04:57 +0800

    add A1_Uart

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <msp430.h>
 #include <sysSetup.h>
+#include <interfaceUart.h>
 
 //  Vin= 1V-3V -----------------
 //     |      |                 |
@@ -19,57 +20,92 @@
 /**
  * hello.c
  */
-#define GPIO//GPIO , LED, FIFO
+#define GPIO // GPIO , LED, FIFO
 
 /*
-* LPM0 等於  close MCLK
-*/
+ * LPM0 等於  close MCLK
+ */
 int main(void)
 {
+    sysSetup();
+    initClockTo16MHz();
+    uartInit(A1_uart,
+             BAUDRATE_9600,
+             NO_PARITY,
+             ONE_STOP_BIT);
+    printk("Select Mode: LED(0x01),GPIO(0x02),FIFO(0x03)");
+
+//    EUSCI_A_UART_transmitData(EUSCI_A1_BASE, 0xDE);
+
+
 #ifdef LED
     sysSetup();
-                                                // Lock CS registers
+    // Lock CS registers
 
-        printf("LED\n");
+    printf("LED\n");
 
-        while (1)
-        {
-            P1OUT ^= BIT0; // Toggle LED
-            __delay_cycles(100000);
-        }
+    while (1)
+    {
+        P1OUT ^= BIT0; // Toggle LED
+        __delay_cycles(100000);
+    }
 
-        return 0;
+    return 0;
 #endif
 
 #ifdef GPIO
-    sysSetup();
-                                                // Lock CS registers
+//    sysSetup();
+    // Lock CS registers
 
+//    printf("GPIO!\n");
+
+    while (1)
+    {
         printf("GPIO!\n");
 
-        while (1)
-        {
-            P1OUT ^= BIT0; // Toggle LED
-            __delay_cycles(100000);
-        }
+        P1OUT ^= BIT0; // Toggle LED
+        __delay_cycles(100000);
+    }
 
-        return 0;
+    return 0;
 #endif
-
 
 #ifdef FIFO
     sysSetup();
-                                                // Lock CS registers
+    // Lock CS registers
 
-        printf("GPIO!\n");
+    printf("GPIO!\n");
 
-        while (1)
-        {
-            P1OUT ^= BIT0; // Toggle LED
-            __delay_cycles(100000);
-        }
+    while (1)
+    {
+        P1OUT ^= BIT0; // Toggle LED
+        __delay_cycles(100000);
+    }
 
-        return 0;
+    return 0;
 #endif
+}
 
+void initClockTo16MHz()
+{
+    // Configure one FRAM waitstate as required by the device datasheet for MCLK
+    // operation beyond 8MHz _before_ configuring the clock system.
+    FRCTL0 = FRCTLPW | NWAITS_1;
+
+    // Clock System Setup
+    CSCTL0_H = CSKEY_H;                     // Unlock CS registers
+    CSCTL1 = DCOFSEL_0;                     // Set DCO to 1MHz
+
+    // Set SMCLK = MCLK = DCO, ACLK = LFXTCLK (VLOCLK if unavailable)
+    CSCTL2 = SELA__LFXTCLK | SELS__DCOCLK | SELM__DCOCLK;
+
+    // Per Device Errata set divider to 4 before changing frequency to
+    // prevent out of spec operation from overshoot transient
+    CSCTL3 = DIVA__4 | DIVS__4 | DIVM__4;   // Set all corresponding clk sources to divide by 4 for errata
+    CSCTL1 = DCOFSEL_4 | DCORSEL;           // Set DCO to 16MHz
+
+    // Delay by ~10us to let DCO settle. 60 cycles = 20 cycles buffer + (10us / (1/4MHz))
+    __delay_cycles(60);
+    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers to 1 for 16MHz operation
+    CSCTL0_H = 0;                           // Lock CS registers
 }
